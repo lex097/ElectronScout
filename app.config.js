1081c642fe3ec0a5c98c1f86246e7fac36d456d9
Aaditya-brrt <denduluriaaditya@gmail.com>
2025-11-01 19:23:04 -0400

    added env and libs

---
+++ app.config.js
@@ -0,0 +1,45 @@
+export default {
+  expo: {
+    extra: {
+      supabaseUrl: process.env.EXPO_PUBLIC_SUPABASE_URL,
+      supabaseAnonKey: process.env.EXPO_PUBLIC_SUPABASE_ANON_KEY
+    },
+    name: "ElectronScout",
+    slug: "ElectronScout",
+    version: "1.0.0",
+    orientation: "portrait",
+    icon: "./assets/images/icon.png",
+    scheme: "electronscout",
+    userInterfaceStyle: "automatic",
+    newArchEnabled: true,
+    splash: {
+      image: "./assets/images/splash-icon.png",
+      resizeMode: "contain",
+      backgroundColor: "#ffffff"
+    },
+    ios: {
+      supportsTablet: true
+    },
+    android: {
+      adaptiveIcon: {
+        foregroundImage: "./assets/images/adaptive-icon.png",
+        backgroundColor: "#ffffff"
+      },
+      edgeToEdgeEnabled: true,
+      predictiveBackGestureEnabled: false
+    },
+    web: {
+      bundler: "metro",
+      output: "static",
+      favicon: "./assets/images/favicon.png"
+    },
+    plugins: [
+      "expo-router",
+      "expo-sqlite"
+    ],
+    experiments: {
+      typedRoutes: true
+    }
+  }
+};
+
